a0138b5b87600f1125942859f0c8b64fd4aca6c7
Jason Yu <jasonyu@csus.edu>
2024-03-13 03:57:30 -0700

    updating some tests to make them pass

--- Savis3/setup-jest.ts
+++ Savis3/setup-jest.ts
@@ -1,5 +1,16 @@
+import { createCanvas, CanvasRenderingContext2D } from 'canvas';
 import 'jest-preset-angular/setup-jest';
 
-export default async function setupJest(){
-    
+declare global {
+  interface HTMLCanvasElement {
+    getContext(contextId: '2d', options?: any): CanvasRenderingContext2D | null;
+  }
 }
+
+HTMLCanvasElement.prototype.getContext = function (contextId: string, options?: any): any {
+  const canvas = createCanvas(0, 0);
+  if (contextId === '2d') {
+    return canvas.getContext('2d', options);
+  }
+  return null;
+};
