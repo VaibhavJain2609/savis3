cded30d500bd18b4275f79d55f4efddced3de6ee
Jason Yu <jasonyu@csus.edu>
2024-03-13 16:02:30 -0700

    reverting some jest configurations

--- Savis3/setup-jest.ts
+++ Savis3/setup-jest.ts
@@ -1,16 +1,5 @@
-import { createCanvas, CanvasRenderingContext2D } from 'canvas';
 import 'jest-preset-angular/setup-jest';
 
-declare global {
-  interface HTMLCanvasElement {
-    getContext(contextId: '2d', options?: any): CanvasRenderingContext2D | null;
-  }
-}
+export default async function setupJest() {
 
-HTMLCanvasElement.prototype.getContext = function (contextId: string, options?: any): any {
-  const canvas = createCanvas(0, 0);
-  if (contextId === '2d') {
-    return canvas.getContext('2d', options);
-  }
-  return null;
-};
+}
