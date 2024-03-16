d38e502673c69047962fd3b701676c768af2a755
Jason Yu <jasonyu@csus.edu>
2024-03-16 16:47:10 -0700

    CRUD operations for DB

--- Savis3/src/app/components/save-load-buttons/load-dialog/load-dialog.component.ts
+++ Savis3/src/app/components/save-load-buttons/load-dialog/load-dialog.component.ts
@@ -1,5 +1,9 @@
 import { Component, Inject, ViewEncapsulation } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
+import { SharedService } from '../../../services/shared.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-load-dialog',
@@ -12,14 +16,60 @@ export class LoadDialogComponent {
   
   constructor(
     public dialog: MatDialog,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private sharedService: SharedService,
+    private af: AngularFireAuth,
+    private firestore: AngularFirestore
     ) { 
       this.files = data.files
-      console.log(this.files)
       this.selectedFile = JSON.stringify(data.files, null, 2)
   }
 
   onNoClick(): void {
     this.dialog.closeAll()
+  }
+
+  loadFile(): void {
+    const selectedData = this.selectedFile.data
+
+    this.sharedService.changeData(selectedData)
+  }
+
+  editFile(): void {
+    const selectedData = this.selectedFile.data
+
+    this.af.authState.pipe(take(1)).subscribe(user => {
+      if (user) {
+        const userId = user.uid
+        this.firestore.collection(`users/${userId}/savedData`, ref => ref.where('fileName', '==', this.selectedFile.fileName))
+          .get()
+          .toPromise()
+          .then(querySnapshot => {
+            if(!querySnapshot.empty) {
+              const doc = querySnapshot.docs[0]
+              doc.ref.update({ data: selectedData })
+            }
+          })
+          .catch(error => {
+            console.error('Error updating document: ', error)
+          })
+      }
+    })
+  }
+
+  deleteFile(): void {
+    this.af.authState.pipe(take(1)).subscribe(user => {
+      if (user) {
+        const userId = user.uid
+        this.firestore.collection(`users/${userId}/savedData`, ref => ref.where('fileName', '==', this.selectedFile.fileName))
+          .get()
+          .toPromise()
+          .then(querySnapshot => {
+            querySnapshot.forEach(doc => {
+              doc.ref.delete()
+            });
+          });
+      }
+    });
   }
 }
