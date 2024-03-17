77af903fd55daaf3189053b91dcecffe11c083f7
Jason Yu <jasonyu@csus.edu>
2024-03-17 15:25:11 -0700

    sign out user from website if they leave the page

--- Savis3/src/app/app.component.ts
+++ Savis3/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Savis3';
+export class AppComponent implements OnInit{
+  constructor(private afAuth: AngularFireAuth) { }
+
+  title = 'Savis3'
+
+  ngOnInit(): void {
+    window.onbeforeunload = () => {
+      this.afAuth.signOut()
+    }
+  }
 }
