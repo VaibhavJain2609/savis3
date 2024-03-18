101ee6f83c4a761c8863b29716a4503b1b8ccd49
Jason Yu <jasonyu@csus.edu>
2024-03-17 20:43:13 -0700

    added a user session & sign out user after 30 mins function

--- Savis3/src/app/app.component.ts
+++ Savis3/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase/app'
 
 @Component({
   selector: 'app-root',
@@ -12,8 +13,21 @@ export class AppComponent implements OnInit{
   title = 'Savis3'
 
   ngOnInit(): void {
-    window.onbeforeunload = () => {
-      this.afAuth.signOut()
-    }
+    this.afAuth.setPersistence(firebase.auth.Auth.Persistence.SESSION)
+      .then(() => {
+        // Check if the session has expired
+        const lastActivity = localStorage.getItem('lastActivity');
+        if (lastActivity && Date.now() - Number(lastActivity) > 30 * 60 * 1000) {
+          // If more than 30 minutes have passed since the last activity, sign out the user
+          this.afAuth.signOut()
+        } else {
+          // Update the timestamp of the last activity
+          localStorage.setItem('lastActivity', Date.now().toString())
+        }
+      })
+      .catch((error) => {
+        var errorCode = error.code
+        var errorMessage = error.message
+      });
   }
 }
