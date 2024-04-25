d2a76ea047d7161c46865e03bf9019e003ac89c7
albinshabu21 <101669542+albinshabu21@users.noreply.github.com>
2024-04-24 17:26:32 -0700

    Shabu/sp 155 (#125)
    * cypress test for correlation
    
    * cypress test for correlation

--- Savis3/src/app/features/correlation/input/input.component.ts
+++ Savis3/src/app/features/correlation/input/input.component.ts
@@ -183,6 +183,7 @@ export class InputComponent implements OnInit {
         resolve(reader.result as string);
       };
     });
+    
   }
 
   async onFileChange(event: any) {
@@ -263,5 +264,7 @@ export class InputComponent implements OnInit {
         this.yValues?.clearValidators();
       },
     });
+    
   }
+ 
 }
