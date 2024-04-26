cc2057ec4650f45fcb1e64e82d9e8d1c89756b81
Jason Yu <jasonyu@csus.edu>
2024-04-25 21:55:55 -0700

    fixing jest configs

--- Savis3/jest.config.js
+++ Savis3/jest.config.js
@@ -15,8 +15,18 @@ module.exports = {
   coverageReporters: [
     'lcov', 'html'
   ],
+  testPathIgnorePatterns: [
+    "/savis3-darwin-x64/",
+    "/savis3-linux-x64/",
+    "/savis3-win32-x64/",
+    "/build/",
+  ],
   coveragePathIgnorePatterns: [
-    "/src/app/Utils/"
+    "/src/app/Utils/",
+    "savis3-darwin-x64/",
+    "savis3-linux-x64/",
+    "savis3-win32-x64/",
+    "build",
   ],
   setupFiles: ['jest-canvas-mock'],
 };
