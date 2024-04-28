e5cd2a3c8155cc8c87f7669c40c14a8c47947ec5
hetvi-patel0 <hetvip2602@gmail.com>
2024-04-28 16:08:01 -0700

    onePropCI cypress test added

--- Savis3/cypress/e2e/onepropci.cy.ts
+++ Savis3/cypress/e2e/onepropci.cy.ts
@@ -7,23 +7,35 @@ describe('One Proprtion Confidence Interval', () => {
     it('passes', () => {
         cy.viewport(1920, 1080)
         cy.visit('localhost:4200/oneproportionCI')
-        cy.get('#sample-size').should('be.disabled')
-        cy.get('#numOfSims').should('be.disabled')
-        cy.get('#runSim').should('be.disabled')
     })
 
-    it('should input fields (Confidence Interval test)', () => {
-        cy.get('#sample-size').should('be.disabled')
-        cy.get('#numOfSims').should('be.disabled')
-        cy.get('#runSim').should('be.disabled')
+    it('should display error message if load button clicked with no inputs', () => {
+        cy.contains('button', 'Load Data').click()
+        cy.on('window:alert', (message) => {
+            expect(message).to.equal('The value of successes and failures must be greater than 0')
+        })
+    })
 
+    it('should input fields (Success and Failure)', () => {
+        cy.get('#success').type('20')
+        cy.get('#failure').type('500')
+        cy.get('.load-button').click()
+    })
+
+    it('should input fields (Run simulations test)', () => {
         cy.get('#success').type('20')
         cy.get('#failure').type('500')
         cy.get('.load-button').click()
 
-        cy.get('#sample-size').should('be.enabled')
-        cy.get('#numOfSims').should('be.enabled')
-        cy.get('#runSim').should('be.enabled')
+        cy.get('#sample-size').type('10')
+        cy.get('#numOfSims').type('10000')
+        cy.get('#runSim').click()
+    })
+
+    it('should input fields (Confidence Interval test)', () => {
+        cy.get('#success').type('20')
+        cy.get('#failure').type('500')
+        cy.get('.load-button').click()
 
         cy.get('#sample-size').type('10')
         cy.get('#numOfSims').type('10000')
@@ -42,17 +54,9 @@ describe('One Proprtion Confidence Interval', () => {
     })
 
     it('should input fields (Min Max test)', () => {
-        cy.get('#sample-size').should('be.disabled')
-        cy.get('#numOfSims').should('be.disabled')
-        cy.get('#runSim').should('be.disabled')
-
         cy.get('#success').type('60')
         cy.get('#failure').type('20')
         cy.get('.load-button').click()
-
-        cy.get('#sample-size').should('be.enabled')
-        cy.get('#numOfSims').should('be.enabled')
-        cy.get('#runSim').should('be.enabled')
 
         cy.get('#sample-size').type('10')
         cy.get('#numOfSims').type('10000')
@@ -61,20 +65,16 @@ describe('One Proprtion Confidence Interval', () => {
         cy.get('#MinMax').click()
         cy.get('#min-interValue').type('{backspace}0.55')
         cy.get('#max-interValue').type('{backspace}0.75')
+
+        cy.get('#includeMin').click()
+        cy.get('#includeMax').click()
+        cy.wait(500)
     })
 
     it('Should reset input fields', () => {
-        cy.get('#sample-size').should('be.disabled')
-        cy.get('#numOfSims').should('be.disabled')
-        cy.get('#runSim').should('be.disabled')
-
         cy.get('#success').type('60')
         cy.get('#failure').type('20')
         cy.get('.load-button').click()
-
-        cy.get('#sample-size').should('be.enabled')
-        cy.get('#numOfSims').should('be.enabled')
-        cy.get('#runSim').should('be.enabled')
 
         cy.get('#sample-size').type('10')
         cy.get('#numOfSims').type('10000')
@@ -85,9 +85,6 @@ describe('One Proprtion Confidence Interval', () => {
         cy.get('#max-interValue').type('{backspace}0.75')
 
         cy.get('.reset-button').click()
-        cy.get('#sample-size').should('be.disabled')
-        cy.get('#numOfSims').should('be.disabled')
-        cy.get('#runSim').should('be.disabled')
     })
 
 })
