e895ecf449d2785002ab64ae77c68b28be395665
vaibhavjain2609 <vaibhavjain@protonmail.com>
2024-05-21 14:03:48 -0700

    updated corrections

--- Savis3/src/app/features/one-mean-ci/one-mean-ci.component.ts
+++ Savis3/src/app/features/one-mean-ci/one-mean-ci.component.ts
@@ -37,7 +37,7 @@ export class OneMeanCIComponent implements OnInit, AfterViewInit, OnDestroy {
   sampleMeans: any[] = []
 
   sampleMeansChartLabel:string = ''
-
+  meanVisible: boolean = false
   scaleChart: any[] = []
 
   inputDataSize: any = NaN
@@ -479,6 +479,7 @@ export class OneMeanCIComponent implements OnInit, AfterViewInit, OnDestroy {
     this.noOfSim = 1
     this.sampleSize = 10
     this.inputDataSize = NaN
+    this.meanVisible = false
     this.inputDataDisplay = ''
     this.sampleDataDisplay = ''
     this.sampleMeansDisplay = ''
@@ -669,8 +670,11 @@ export class OneMeanCIComponent implements OnInit, AfterViewInit, OnDestroy {
         dataChart = this.setDataFromRaw(dataChart, ([chosen, unchosen]))
         
         dataDisplay = dataArray.reduce(
-          (acc, x, idx) => acc + `${idx + 1}`.padEnd(8, ' ') + `${x} σ: ${this.sampleStds[idx]}`.padEnd(25, ' ') + `${this.translate.instant('dotPlot_mean')} ${'s'}\n`,
-          `ID`.padEnd(8, ' ') + `${this.translate.instant('omci_values')} s\n`
+          (acc, x, idx) => acc + 
+            `${(idx + 1).toString().padEnd(8, ' ')}` + 
+            `${x.toString().padEnd(8, ' ')}` + 
+            `${this.sampleStds[idx].toString().padEnd(25, ' ')}\n`,
+          `ID`.padEnd(8, ' ') + `mean`.padEnd(8, ' ') + `s\n`
         );
       }
 
@@ -1135,7 +1139,7 @@ export class OneMeanCIComponent implements OnInit, AfterViewInit, OnDestroy {
     }
     this.lowerBounds = lowers
     this.upperBounds = uppers
-
+    this.meanVisible = true
     it++
     tmp = inInterval.pop()
     tmp = notInInterval.pop()
